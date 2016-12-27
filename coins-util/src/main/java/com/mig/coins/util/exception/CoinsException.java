60b4d214989b8ee7c0858c9a7edd64cafc0f2d5b
maantonr <maantonr@gmail.com>
2016-12-27 11:43:58 +0100

    Primera version

---
+++ coins-util/src/main/java/com/mig/coins/util/exception/CoinsException.java
@@ -0,0 +1,9 @@
+package com.mig.coins.util.exception;
+
+// PDTE Documentar
+// PDTE Implementar
+public class CoinsException extends Exception {
+
+	private static final long serialVersionUID = 3945849173114436822L;
+
+}
